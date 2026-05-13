3c8baaa181cdf5e8ec6979170163aab3ba4c1a55
Deyvi Lizandro Rivera <deyvi2413@gmail.com>
2026-05-13 08:47:26 -0500

    Cmabio de neon a SUperbase

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+        conn_max_age=0,
         conn_health_checks=True,
     )
 }
