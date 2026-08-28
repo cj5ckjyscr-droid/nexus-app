ecbbadbf0b6bab745919375b110b4d1154fdbba5
Deyvi10 <deyvi2413@gmail.com>
2026-08-27 23:27:04 -0500

    cambios por errores

---
+++ core/migrations/0024_merge_20260827.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0021_configuracion_logo_sistema'),
+        ('core', '0023_perfil_cedula'),
+    ]
+
+    operations = [
+    ]
